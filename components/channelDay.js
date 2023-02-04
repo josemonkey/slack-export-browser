d45b2b0e98a35cf3a9208f569f9470d17a2d8c94
josemonkey <josemonkey@josemonkey.com>
2023-02-04 07:21:26 -0500

    hacking

--- components/channelDay.js
+++ components/channelDay.js
@@ -1,20 +1,32 @@
 import utilStyles from '../styles/utils.module.css'
 import Date from './date'
+import MessageBlock from './messageBlock';
 
 
 
 export default function ChannelDay({ id, date, content }) {
 
+    console.log(content);
     return (
         <div>
 
             <h2><Date dateString={date} /></h2>
 
-            {content.map(({ type, text, user, blocks }) => (
+
+            {content?.map((thisOne) => (
+
+
+
                 <div>
-                    {type}<br />
-                    <div dangerouslySetInnerHTML={{ __html: text }} />
-                    {user}<br />
+                    {thisOne.type}<br />
+                    <div dangerouslySetInnerHTML={{ __html: thisOne.text }} />
+                    {thisOne.user}<br />
+
+                    {thisOne.blocks?.map((block) => (
+
+                            <MessageBlock block={block} />
+                    ))}
+
 
                 </div>
             ))}
---
+++ components/messageBlock.js
@@ -0,0 +1,13 @@
+export default function MessageBlock({ block }) {
+
+    console.log(block);
+    return (
+
+
+
+
+        <div>{block.type}</div>
+
+
+    );
+}
