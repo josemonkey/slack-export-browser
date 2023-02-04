d01d1c6be931beb395ae827fd9e9501699b66e38
josemonkey <josemonkey@josemonkey.com>
2023-02-04 14:18:58 -0500

    Some formatting

--- components/richTextBlock.js
+++ components/richTextBlock.js
@@ -1,14 +1,15 @@
 import RichTextElement from "./richTextElement";
+import styles from './richTextBlock.module.css';
 
 export default function RichTextBlock({ username, block }) {
 
 
     return (
 
-        <div>
-            <div style={{float: "left", marginRight: "5px"}}><b>{username}:</b> </div>
+        <div className={styles.flexContainer}>
+            <div className={styles.flexChild}><b>{username}:</b> </div>
 
-            <div>
+            <div className={styles.flexChild}>
             {block.elements?.map((element) => (
 
 
