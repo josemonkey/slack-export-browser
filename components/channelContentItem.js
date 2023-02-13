a68d9fc9da04274a0d8042a62730478dac176909
Jeremy Thompson <josemonkey@josemonkey.com>
2023-02-13 11:19:54 -0500

    Minor cleanup on user code

--- components/channelContentItem.js
+++ components/channelContentItem.js
@@ -2,17 +2,19 @@ import utilStyles from '../styles/utils.module.css'
 import Message from './message'
 import User from './user'
 
-
-
 export default function ChannelContentItem({
   contentItem,
   allChannels,
   allUsers,
 }) {
-
   if (contentItem.type === 'message') {
     if (contentItem.subtype && contentItem.subtype === 'channel_join') {
-      return <div><User userData={allUsers} userId={contentItem.user}/> joined the channel</div>
+      return (
+        <div>
+          <User userData={allUsers} userId={contentItem.user} /> joined the
+          channel
+        </div>
+      )
       // FIXME
     } else if (
       contentItem.subtype &&
@@ -20,7 +22,8 @@ export default function ChannelContentItem({
     ) {
       return (
         <div>
-          <User userData={allUsers} userId={contentItem.user}/> set the channel purpose: {contentItem.purpose}
+          <User userData={allUsers} userId={contentItem.user} /> set the channel
+          purpose: {contentItem.purpose}
         </div>
       )
       // FIXME
--- components/user.js
+++ components/user.js
@@ -1,47 +1,11 @@
-import { getUsers } from "../lib/users";
-
-function getUserName(userData, userId) {
-  var result = userData.filter((user) => {
-    return user.id === userId
-  })
-
-  if (result && result.length > 0) {
-    return result[0].name
-  } else {
-    return '[UNKNOWN USER]'
-  }
-}
-
-function getUser(userData, userId) {
-  var result = userData.filter((user) => {
-    return user.id === userId
-  })
-
-  if (result && result.length > 0) {
-    return result[0]
-  } else {
-    return { name: "[UNKNOWN USER]" };
-  }
-}
-
-
-export async function getStaticProps() {
-    const userData= await getUsers();
-    return {
-      props: {
-        userData,
-      },
-    }
-  }
+import { getUsers, getUser } from '../lib/users'
 
 export default function User({ userData, userId }) {
+  const thisUser = getUser(userData, userId)
 
-
-  const thisUser = getUser(userData, userId);
-
-    return (
-
-    <span><b>{thisUser.name}</b></span>
-    )
-  }
-  
+  return (
+    <span>
+      <b>{thisUser.name}</b>
+    </span>
+  )
+}
